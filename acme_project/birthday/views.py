8c73f712d4d70b3acb5f868a77f41eda3ecc5824
Aleksandr Tekutev <tekutiev.sasha@gmail.com>
2023-05-30 12:11:08 +0100

    Added edit/delete functions

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,20 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
 
 from .forms import BirthdayForm
 from .utils import calculate_birthday_countdown
 from .models import Birthday
 
 
-def birthday(request):
-    form = BirthdayForm(request.POST or None)
+def birthday(request, pk=None):
+    if pk is not None:
+        instance = get_object_or_404(Birthday, pk=pk)
+    else:
+        instance = None
+    form = BirthdayForm(request.POST or None, instance=instance)
     context = {'form': form}
-    birthday_countdown = None
     if form.is_valid():
         form.save()
         birthday_countdown = calculate_birthday_countdown(
             form.cleaned_data['birthday']
         )
-    context.update({'birthday_countdown': birthday_countdown})
+        context.update({'birthday_countdown': birthday_countdown})
     return render(request, 'birthday/birthday.html', context)
 
 
@@ -22,3 +25,20 @@ def birthday_list(request):
     birthdays = Birthday.objects.all()
     context = {'birthdays': birthdays}
     return render(request, 'birthday/birthday_list.html', context)
+
+
+def delete_birthday(request, pk):
+    # Получаем объект модели или выбрасываем 404 ошибку.
+    instance = get_object_or_404(Birthday, pk=pk)
+    # В форму передаём только объект модели;
+    # передавать в форму параметры запроса не нужно.
+    form = BirthdayForm(instance=instance)
+    context = {'form': form}
+    # Если был получен POST-запрос...
+    if request.method == 'POST':
+        # ...удаляем объект:
+        instance.delete()
+        # ...и переадресовываем пользователя на страницу со списком записей.
+        return redirect('birthday:list')
+    # Если был получен GET-запрос — отображаем форму.
+    return render(request, 'birthday/birthday.html', context)
