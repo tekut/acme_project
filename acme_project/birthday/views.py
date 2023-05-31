d1f016b050f39aa29e9075d050a428fa72754e0d
Aleksandr Tekutev <tekutiev.sasha@gmail.com>
2023-05-31 10:03:37 +0100

    Updates

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -4,6 +4,8 @@ from .forms import BirthdayForm
 from .utils import calculate_birthday_countdown
 from .models import Birthday
 
+from django.core.paginator import Paginator
+
 
 def birthday(request, pk=None):
     if pk is not None:
@@ -26,8 +28,11 @@ def birthday(request, pk=None):
 
 
 def birthday_list(request):
-    birthdays = Birthday.objects.all()
-    context = {'birthdays': birthdays}
+    birthdays = Birthday.objects.order_by('id')
+    paginator = Paginator(birthdays, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'page_obj': page_obj}
     return render(request, 'birthday/birthday_list.html', context)
 
 
