90e991193af3f65fd60a3217fae9512863ebaf02
Aleksandr Tekutev <tekutiev.sasha@gmail.com>
2023-06-05 09:31:20 +0100

    Finished sprint

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -42,6 +42,7 @@ def add_comment(request, pk):
 
 class BirthdayListView(ListView):
     model = Birthday
+    queryset = Birthday.objects.prefetch_related('tags').select_related('author')
     ordering = 'id'
     paginate_by = 10
 
